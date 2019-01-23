c4318b381926e8d94edbff34de96065aa74e9adb
kaito.shibuya <kaito.shibuya@gaiax.com>
2019-01-23 18:00:19 +0900

    doc2vecの結果を返す

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 
 import os
 import json
+from os import path
 
 from flask import Flask, request, jsonify
+from gensim.models.doc2vec import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
 
 
 app = Flask(__name__)
@@ -19,12 +22,18 @@ def doc2vec():
     if request.method == 'POST':
         data = request.data.decode('utf-8')
         data = json.loads(data)
-        data = str(data['key'])
+        documents = data['documents']
+
+    tags = [f'doc{i+1}' for i in range(0, len(documents))]
+    train_docs = [TaggedDocument(words=words, tags=[tag]) for words, tag in zip(documents, tags)]
+    model = Doc2Vec(documents=train_docs, min_count=1, dm=0)
+
+    result = {tag: model.docvecs.most_similar(tag) for tag in tags}
 
     # 結果を JSON にして返す
     return jsonify(
             {
-                'result': data
+                'result': result
             }
         )
 
