2241ffe855a1759d8cf6d709ed4bcd903c36d809
kaito.shibuya <kaito.shibuya@gaiax.com>
2019-01-23 13:55:35 +0900

    jsonで受け取ってjsonで返す窓口の作成

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/doc2vec', methods=['POST'])
+def doc2vec():
+    if request.method == 'POST':
+        data = request.data.decode('utf-8')
+        data = json.loads(data)
+        data = str(data['key'])
+
+    # 結果を JSON にして返す
+    return jsonify(
+            {
+                'result': data
+            }
+        )
 
 if __name__ == '__main__':
     app.run()
