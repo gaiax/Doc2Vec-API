e69fdf6f95a2da2040e2bb8e01f4eb21ff586946
kaito.shibuya <kaito.shibuya@gaiax.com>
2019-01-23 18:16:25 +0900

    janomeで分ち書きに対応

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import json
 from os import path
 
 from flask import Flask, request, jsonify
+from janome.tokenizer import Tokenizer
 from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
+
+def words(doc):
+    t = Tokenizer()
+    return [token.surface for token in t.tokenize(doc) if token.part_of_speech.split(',')[0] in ['名詞']]
 
 
 @app.route('/')
@@ -25,7 +31,7 @@ def doc2vec():
         documents = data['documents']
 
     tags = [f'doc{i+1}' for i in range(0, len(documents))]
-    train_docs = [TaggedDocument(words=words, tags=[tag]) for words, tag in zip(documents, tags)]
+    train_docs = [TaggedDocument(words=words(document), tags=[tag]) for document, tag in zip(documents, tags)]
     model = Doc2Vec(documents=train_docs, min_count=1, dm=0)
 
     result = {tag: {"document": document, "distances": model.docvecs.most_similar(tag)} for document, tag in zip(documents, tags)}
